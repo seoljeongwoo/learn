1b83d67ed91dddf0f5b3a2b8a13f4efea9fd3c28
Seol Jeong Woo <62869207+seoljeongwoo@users.noreply.github.com>
2021-01-17 00:17:41 +0900

    Add files via upload

--- algorithm/BOJ_17413.py
+++ algorithm/BOJ_17413.py
@@ -1,27 +1,25 @@
-S = input()
-tag ,result = False, ''
-new_string =''
-for i in range(len(S)):
-    if tag == True:
-        result += S[i]
-        if S[i] == '>':
-            tag = False
-    else:
-        if S[i] == '<':
-            if len(new_string) !=0:
-                result += new_string[::-1]
-                new_string = ''
+import sys
+input = sys.stdin.readline
+direction = [(0,1),(0,-1),(1,0),(-1,0)]
+R,C=map(int,input().split())
+farm = []
+for i in range(R):
+    farm.append(list(input()))
+wolfcheck = True
+for i in range(R):
+    for j in range(C):
+        if farm[i][j] == 'W':
+            for dx , dy  in direction:
+                nx ,ny = i+dx,j+dy
+                if 0<=nx <R and 0<=ny<C:
+                    if farm[nx][ny] =='S':
+                        wolfcheck = False
+                    elif farm[nx][ny] == '.':
+                        farm[nx][ny] = 'D'
+if wolfcheck:
+    print(1)
+    for i in range(len(farm)):
+        print(''.join(map(str,farm[i])))
+else:
+    print(0)
 
-            result += S[i]
-            tag = True
-        elif S[i] == ' ':
-            if len(new_string) != 0:
-                result += new_string[::-1]
-                new_string = ''
-
-            result += S[i]
-        else:
-            new_string += S[i]
-if len(new_string) != 0:
-    result += new_string[::-1]
-print(result)
